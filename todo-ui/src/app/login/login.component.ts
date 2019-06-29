78df8143839774d227726c53801c27e9535a0e14
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-06-29 21:28:49 +0530

    v4 changes login

--- todo-ui/src/app/login/login.component.ts
+++ todo-ui/src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'vijay';
+  password = 'password';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleLogin(){
+
   }
 
 }
