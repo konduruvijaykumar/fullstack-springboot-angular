a42080fa991392eba57de637bcb2347a06f02f00
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-06-30 09:35:09 +0530

    v5 route changes

--- todo-ui/src/app/app-routing.module.ts
+++ todo-ui/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent }
+];
 
 @NgModule({
   declarations: [],
