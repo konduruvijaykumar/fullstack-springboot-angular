0b78fe40390eb9a2a37e68d92fb72b68933ccf74
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-03 13:35:37 +0530

    v8 route guard & redirect login

--- todo-ui/src/app/service/route-guard.service.ts
+++ todo-ui/src/app/service/route-guard.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { LocalAuthenticationService } from './local-authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService {
+export class RouteGuardService implements CanActivate {
 
-  constructor() { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.localAuthenticationService.isUserLoggedIn()) {
+      return true;
+    }
+    this.router.navigate(['login']);
+    return false;
+  }
+
+  constructor(private localAuthenticationService: LocalAuthenticationService, private router: Router) { }
 }
