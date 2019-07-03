b8368378bd2a0586d7f12afa228a5c70313abfe6
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-03 11:52:09 +0530

    v7 auth logic

--- todo-ui/src/app/login/login.component.ts
+++ todo-ui/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LocalAuthenticationService } from '../service/local-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
   // Dependency injection is a built in faeture, just declare it as constructor argument.
   // In type script variable passed in constructor is available as member variable.
   // Making private will not be visible outside class
-  constructor(private router: Router) { }
+  constructor(private router: Router, private localAuthenticationService: LocalAuthenticationService) { }
 
   ngOnInit() {
   }
@@ -24,8 +25,9 @@ export class LoginComponent implements OnInit {
   handleLogin() {
     // console.log('username: ' + this.username);
     // console.log('password: ' + this.password);
-    if (this.username === 'vijay' && this.password === 'password') {
+    if (this.localAuthenticationService.authenticate(this.username, this.password)) {
       this.invalidLogin = false;
+      // sessionStorage.setItem('authenticatedUser', this.username);
       // Redirect to welcome page.
       this.router.navigate(['welcome', this.username]);
     } else {
--- todo-ui/src/app/service/local-authentication.service.ts
+++ todo-ui/src/app/service/local-authentication.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ALLOW_MULTIPLE_PLATFORMS } from '@angular/core/src/application_ref';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,18 @@ import { Injectable } from '@angular/core';
 export class LocalAuthenticationService {
 
   constructor() { }
+
+  authenticate(username: string, password: string) {
+    if (username === 'vijay' && password === 'password') {
+      sessionStorage.setItem('authenticatedUser', username);
+      return true;
+    }
+    return false;
+  }
+
+  isUserLoggedIn() {
+    const user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
+  }
+
 }
