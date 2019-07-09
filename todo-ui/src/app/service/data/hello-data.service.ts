3aefc154e4651a7c82185119d8ac1137b8318664
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-09 18:16:10 +0530

    v11 preflight request issue

--- todo-ui/src/app/service/data/hello-data.service.ts
+++ todo-ui/src/app/service/data/hello-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +13,25 @@ export class HelloDataService {
     return this.httpClient.get<Hello>('http://localhost:8080/hello-bean');
   }
 
+  // Note: After adding form based and basic auth to spring rest service resources, we will not be able to get data.
+  // To fix it we will send basic auth data as headers for API calls.
+  // It is not a better practice to add basic auth header to all requests. We will later use Interceptors to that for all requests.
   callHelloPathVariableService(name: string) {
     // The below quotes are not single quotes, they are actually backtick or backquote
     // Available below esc button. This allows to use variables
-    return this.httpClient.get<Hello>(`http://localhost:8080/hello-path-param/${name}`);
+    const basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+    const headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    });
+    // When passing headers with differnt name object like header, then use {headers: header}. If the name is headers then use {headers}.
+    return this.httpClient.get<Hello>(`http://localhost:8080/hello-path-param/${name}`, {headers});
+  }
+
+  createBasicAuthenticationHttpHeader() {
+    const username = 'vijay';
+    const password = 'password';
+    const basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    return basicAuthHeaderString;
   }
 
 }
