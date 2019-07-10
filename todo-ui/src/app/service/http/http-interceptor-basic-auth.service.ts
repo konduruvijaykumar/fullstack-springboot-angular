019160ed76ab26c547d9838a0b3c73af106ca6fb
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-10 07:29:27 +0530

    v11 basic auth integration

--- todo-ui/src/app/service/http/http-interceptor-basic-auth.service.ts
+++ todo-ui/src/app/service/http/http-interceptor-basic-auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpInterceptorBasicAuthService {
+export class HttpInterceptorBasicAuthService implements HttpInterceptor{
 
   constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const username = 'vijay';
+    const password = 'password';
+    const basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    // request objects cannot be modified, hence we clone override a specific property
+    req = req.clone({
+      setHeaders: {
+        Authorization: basicAuthHeaderString
+      }
+    });
+
+    return next.handle(req);
+  }
+
 }
