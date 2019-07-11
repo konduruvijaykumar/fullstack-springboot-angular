2990ac0a93581f353cf5cf6bb35f3995b803d63c
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-11 10:25:05 +0530

    v12 jwt service initial commit

---
+++ todo-jwt-service/src/main/java/org/pjay/todo/TodoJwtServiceApplication.java
@@ -0,0 +1,13 @@
+package org.pjay.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJwtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJwtServiceApplication.class, args);
+	}
+
+}
